bcc44f717412a1280a14d2f806bf482dccd3a8ec
vishalkumar905 <61614359+vishalkumar905@users.noreply.github.com>
2026-06-14 19:19:01 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            const need = target - nums[i];
+            if (need in map) {
+                return [map[need], i];
+            }
+
+            map[nums[i]] = i;
+        }
+
+    }
+}
